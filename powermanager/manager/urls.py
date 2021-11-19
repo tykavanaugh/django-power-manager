b23fd7da267dec61defc28c5991d263d63f4d39c
tykavanaugh <tykavanaugh@gmail.com>
2021-11-19 13:00:54 -0500

    Starting urls, no placeholder views yet

--- powermanager/manager/urls.py
+++ powermanager/manager/urls.py
@@ -1,11 +1,17 @@
 
 from django.urls import path,include
-
-
 from manager import views
 
 app_name = 'manager'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name='index'),
+    path("home/", views.home_page, name='home'),
+    path("view/", views.view_page, name='view'),
+    path("edit/", views.edit_page, name='edit'),
+    
+    path("auth/", include('django.contrib.auth.urls')),
+    
+    # Sign up a new user
+    path("auth/signup/", views.signup_page, name="signup")
 ]
