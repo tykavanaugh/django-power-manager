4bb07ca4ff43fd306b34906ed4d3977692b205c1
tykavanaugh <tykavanaugh@gmail.com>
2021-11-22 09:04:59 -0600

    More heroku

--- powermanager/powermanager/settings.py
+++ powermanager/powermanager/settings.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -133,3 +133,4 @@ LOGOUT_REDIRECT_URL = "manager:login"
 
 import django_heroku
 django_heroku.settings(locals())
+
